25c83ba8a7fb7393237218604b65fd452377ff65
ervenus-dott <120420594+ervenus-dott@users.noreply.github.com>
2025-03-04 21:02:25 -0700

    hello world

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+const PIG_COUNT: i32 = 28;
+// const TAU: f32 = 6.28;
+
+fn main() {
+    let mut goat_count: i64 = 5;
+    let apple_count = 5;
+    println!("Hello, world!");
+    goat_count += 2000000000;
+    println!("we have {goat_count} goats and {apple_count} apples and {PIG_COUNT} pigs");
+}
