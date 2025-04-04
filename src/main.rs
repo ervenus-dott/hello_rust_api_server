ae74a659f4cf87a31ab6f2e18127004bee4f06cc
ervenus-dott <120420594+ervenus-dott@users.noreply.github.com>
2025-04-03 22:02:40 -0600

    add `/calendar` endpoint that parses query string parameters and returns a json string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use actix_cors::Cors;
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+use actix_web::web::Query;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Calendar {
+    name: String,
+    description: String,
+    start: String,
+    end: String,
+}
+
 const PIG_COUNT: i32 = 28;
 // const TAU: f32 = 6.28;
 
@@ -21,6 +32,13 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+#[get("/calendar")]
+async fn calendar(query: Query<Calendar>) -> impl Responder {
+    let calendar = query.into_inner();
+    println!("/calendar was visited: {:?}", calendar);
+    HttpResponse::Ok().body(serde_json::to_string(&calendar).unwrap())
+}
+
 async fn manual_hello() -> impl Responder {
     println!("/hey was visited");
     HttpResponse::Ok().body("Hey there!")
@@ -38,6 +56,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(cors)
             .service(hello)
+            .service(calendar)
             .service(goats)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
